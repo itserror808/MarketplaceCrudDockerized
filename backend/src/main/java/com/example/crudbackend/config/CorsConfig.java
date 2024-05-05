14a05ce44d443d22199e710909da9451c8434ef3
MEFTAH Ahmed Reda <meftahahmedreda02@gmail.com>
2024-05-05 02:07:57 +0100

    hello hmeeeed

--- backend/src/main/java/com/example/crudbackend/config/CorsConfig.java
+++ backend/src/main/java/com/example/crudbackend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("*")
+                    .allowedOrigins("https://marketplace-crud-dockerized.vercel.app/")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                     .allowedHeaders("*")
                     .allowCredentials(true);
